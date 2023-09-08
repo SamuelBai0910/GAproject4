bd79f0546a6534986d94aab5c75695550cd4661b
SamuelBai0910 <baisimo2022@163.com>
2023-09-08 15:59:51 +1000

    update react-quill layout

--- src/pages/CreatePostPage/CreatePostPage.jsx
+++ src/pages/CreatePostPage/CreatePostPage.jsx
@@ -1,4 +1,7 @@
 import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './CreatePostPage.css';
+
 export default function CreatePostPage() {
   return(
     <form action="">
@@ -6,6 +9,7 @@ export default function CreatePostPage() {
       <input type="summary" placeholder={'Summary'} />
       <input type="file" />
       <ReactQuill />
+      <button>Create Post</button>
     </form>
   )
 }
